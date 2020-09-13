95c6d11fdf5e9e231845b3c7955299f552b55bbf
Lachlan Court <lachlan.m.court@gmail.com>
2020-09-13 11:59:02 +1000

    Comments

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,10 +1,19 @@
-///cygdrive/C/Users/lachl/OneDrive/Documents/Uni/SENG1120_Assessment/Assignment1
+/*************************************
+**** Lachlan Court
+**** c3308061
+**** 13/09/2020
+**** SENG1120 Assignment 1
+**************************************/
+
+
+//cd /cygdrive/C/Users/lachl/OneDrive/Documents/Uni/SENG1120_Assessment/Assignment1
 
 #include <iostream>
 using namespace std;
 #include "LinkedList.h"
 typedef Node::value_type value_type;
 
+// Constructor, initialise empty list
 LinkedList::LinkedList()
 {
 	head = NULL;
@@ -12,187 +21,278 @@ LinkedList::LinkedList()
 	current = NULL;
 	size = 0;
 }
-LinkedList::~LinkedList()
-{}
 
+// Deconstructor
+LinkedList::~LinkedList()
+{
+	// Loop through until the head is not NULL and delete the head each time
+	while (head != NULL)
+	{	
+		// Keep track of the item in the list after the head
+		current = head->getNext();
+		// Delete head
+		delete(head);
+		// Set the head to what used to be the second item in the list
+		head = current;
+	}
+}
+
+// Query the size of the list
 int LinkedList::getSize() const
 {
 	return size;
 }
 
-void LinkedList::addToTail(value_type* data_)
-{
-	if ((head == NULL) && (tail == NULL))
-	{
-		head = new Node(data_);
-		tail = head;
-		size++;
-	}
-	else
-	{
-		current = new Node(data_);
-		current->setPrev(tail);
-		tail->setNext(current);
-		tail = current;
-		size++;
-	}
-}
-
+// Receives a string and adds the elements to the Linked List delimetered by spaces
 void LinkedList::add(value_type data)
 {
+	// Declare a temp string which will hold the value of the string iterated so far
 	value_type* temp = new value_type();
+	// Loop through the parsed data one character at a time
 	for (long unsigned int i = 0; i < data.length(); i++)
 	{
+		// If the charater is a space, the word will be added to the list
 		if (data[i] == ' ')
 		{
-			cout << *temp;
 			addToTail(temp);
+			// Reset temp ready for a new word
 			temp = new value_type();
 		}
 		else
 		{
+			// If the character is not a space, add the character to temp
 			*temp += data[i];
 		}
 	}
+	// The last word doesn't end in a space, so it needs to be added explicitly 
 	addToTail(temp);
-	temp = NULL;
 }
 
-void LinkedList::addToHead(value_type* data_)
+// Receives a string that will be added to the Linked List at the end
+void LinkedList::addToTail(value_type* data_)
 {
+	// Check if the list is empty
 	if ((head == NULL) && (tail == NULL))
 	{
-		tail = new Node(data_);
-		head = tail;
+		// Create a new node with the specified data
+		head = new Node(data_);
+		// The list has one item so the tail and head are the same node
+		tail = head;
+		// Increase the size of the list
 		size++;
 	}
 	else
 	{
+		// Create a new node with the specified data
 		current = new Node(data_);
-		current->setNext(head);
-		head->setPrev(current);
-		head = current;
+		// Link the new node's previous to the tail of the list
+		current->setPrev(tail);
+		// Link the tail's next to the new node
+		tail->setNext(current);
+		// Set the tail to the new node
+		tail = current;
+		// Having just manipulated the current, reset it to head
+		jumpToHead();
+		// Increase the size of the list
 		size++;
 	}
 }
 
+// Receives a string that will be added to the Linked List at the start
+void LinkedList::addToHead(value_type* data_)
+{
+	// Check if the list is empty
+	if ((head == NULL) && (tail == NULL))
+	{
+		// Create a new node with the specified data
+		tail = new Node(data_);
+		// The list has one item so the tail and head are the same node
+		head = tail;
+		// Increase the size of the list
+		size++;
+	}
+	else
+	{
+		// Create a new node with the specified data
+		current = new Node(data_);
+		// Link the new node's next to the head of the list
+		current->setNext(head);
+		// Link the head's previous to the new node
+		head->setPrev(current);
+		// Set the head to the new node
+		head = current;
+		// Increase the size of the list
+		size++;
+	}
+}
+
+// Receives a string that will be removed from the Linked List
 void LinkedList::remove(value_type identifier)
 {
+	// Check if the list is empty and return
 	if ((head == NULL) && (tail == NULL))
 	{
 		return;
 	}
+	
+	// Define a value to keep track of whether the identifier has been found in the Linked List
 	bool identified;
-	for (current = head; current != NULL; moveNext())
+	// Loop through the Linked List
+	for (jumpToHead(); current != NULL; moveNext())
 	{
+		// Check if the identifier is the value of the current position in the Linked List
 		if (identifier == *getCurrent())
 		{
 			identified = true;
 			
+			// If there is one item in the list
 			if (head == tail)
 			{
-				current = head;
+				// Reset the head and tail as the list is about to become empty
 				head = NULL;
 				tail = NULL;
 			}
+			// If the first item in the list
 			else if (current == head)
 			{
+				// Set the head to the second item in the list as the first is about to be removed
 				head = current->getNext();
 				head->setPrev(NULL);
 		
 			}
+			// If the last item in the list
 			else if (current == tail)
 			{
+				// Set the tail to the second last item in the list as the last is about to be removed
 				tail = current->getPrev();
 				tail->setNext(NULL);
 		
 			}
+			// The list has more than one item and the item to be removed is not at the head or tail
 			else
 			{
+				// Link the node before the current to the node after the current
 				current->getPrev()->setNext(current->getNext());
 				current->getNext()->setPrev(current->getPrev());
 			}
+			// Delete the current and decrement the size
 			delete(current);
+			size--;
+			// current has just been deleted so reset it to the head and start the loop over
 			jumpToHead();
 		}
 	}
 	
+	// If no item was found, return
 	if (!identified)
 	{
-		cout << "No such item in list" << endl;
+		cout << "No such item in list";
 		return;
 	}
 }
 
+// Receives a string and counts how many occurrences of that string are in the Linked List
 int LinkedList::count(const value_type identifier)
 {
 	int occurrences = 0;
+	// jumpToHead returns -1 if the Linked list is empty
 	if (jumpToHead() == -1)
 	{
 		return -1;
 	}
-	for (current = head; current != NULL; moveNext())
+	// Loop through the Linked List
+	for (jumpToHead(); current != NULL; moveNext())
 	{
+		// Check if the identifier is the value of the current position in the Linked List
 		if (identifier == *getCurrent())
 		{
+			// Increment the counter
 			occurrences++;
 		}
 	}
+	// Reset the current to head as it has just been manipulated
+	jumpToHead();
+	// Return the number of occurrences
 	return occurrences;
 }
 
+// Sorts the Linked List in alphabetical order
 void LinkedList::sort()
 {
+	// Two Nodes will be picked out at a time to be checked
 	value_type a;
 	value_type b;
+	// After a swap, the loop will be restarted
 	bool restart = false;
-
-	for (current = head; current->getNext() != NULL; moveNext())
+	
+	// Check if the Linked List is empty
+	if ((head == NULL) && (tail == NULL))
 	{
+		return;
+	}
+
+	// Loop through the Linked List
+	for (jumpToHead(); current->getNext() != NULL; moveNext())
+	{
+		// If a swap occured last iteration, reset the current to the head
 		if (restart)
 		{
 			jumpToHead();
 			restart = false;
 		}
+		
+		// Pick out the data from two nodes to compare
 		a = *current->getData();
 		b = *current->getNext()->getData();
 		
-		
+		// Determine which of the two words is shorter
 		int smallest = (int)a.length();
 		if ((int)b.length() < smallest)
 		{
 			smallest = b.length();
 		}
 		
+		// Loop through the length of the smaller word
 		int i;
 		for (i = 0; i < smallest; i++)
 		{
+			// Keep checking until a character that is unique in the two words is found
 			if ((int)a[i] != (int)b[i])
 			{
 				break;
 			}
 		}
+		// If the for loop above has reached the length of the smallest word, a swap is not necessary as they are the same (Or the larger word starts with the smaller word)
 		if (i == smallest)
 		{
 			continue;
 		}
+		// Now that a unique character has been found, determine if a swap is necessary by comparing ASCII codes
 		if ((int)a[i] > (int)b[i])
 		{
+			// Swap the two nodes
 			swap(current, current->getNext());
 			
+			// If the first word has been swapped, reset the head
 			if (current == head)
 			{
 				head = current->getPrev();
 			}
+			// Ensure that current is reset at the start of the for loop
 			restart = true;
+			// Reset current now as well, in case current is currently NULL
 			jumpToHead();
 		}
 	}
+	// As current has been manipulated, ensure it is reset to the head
+	jumpToHead();
 }
 
+// Resets current to head
 int LinkedList::jumpToHead()
 {
 	current = head;
+	// Return -1 if current is now NULL - indicating list is empty
 	if (current == NULL)
 	{
 		return -1;
@@ -200,9 +300,11 @@ int LinkedList::jumpToHead()
 	return 0;
 }
 
+// Resets current to tail
 int LinkedList::jumpToTail()
 {
 	current = tail;
+	// Return -1 if current is now NULL - indicating list is empty
 	if (current == NULL)
 	{
 		return -1;
@@ -210,9 +312,11 @@ int LinkedList::jumpToTail()
 	return 0;
 }
 
+// Moves current to the next Node in the list
 int LinkedList::moveNext()
 {
 	current = current->getNext();
+	// Return -1 if current is now NULL - reached the end of the list
 	if (current == NULL)
 	{
 		return -1;
@@ -220,9 +324,11 @@ int LinkedList::moveNext()
 	return 0;
 }
 
+// Moves current to the previous Node in the list
 int LinkedList::movePrev()
 {
 	current = current->getPrev();
+	// Return -1 if current is now NULL - reached the start of the list
 	if (current == NULL)
 	{
 		return -1;
@@ -230,52 +336,72 @@ int LinkedList::movePrev()
 	return 0;
 }
 
+// Return the data stored in the current Node
 value_type* LinkedList::getCurrent() const
 {
 	return current->getData();
 }
 
-void LinkedList::operator += (LinkedList& other)
+// Add the otherList data onto the existing Linked List
+void LinkedList::operator += (LinkedList& otherList)
 {
-	if (other.jumpToHead() == -1)
+	// Check if otherList is empty and also move otherList's current to head
+	if (otherList.jumpToHead() == -1)
 	{
 		return;
 	}
+	// Loop through otherList
 	do
 	{
-		value_type* temp = new value_type(*other.getCurrent());
+		// Create a new string with the same value as the Node in otherList
+		value_type* temp = new value_type(*otherList.getCurrent());
+		// Add new string to the existing Linked List
 		addToTail(temp);
 	}
-	while (other.moveNext() == 0);
+	// Loop until the end of otherList is reached
+	while (otherList.moveNext() == 0);
 }
 
-
+// Output the Linked List as a sentence
 ostream& operator << (ostream& out, LinkedList& temp)
 {
+	// Set current to head, return if the Linked List is empty
 	if (temp.jumpToHead() == -1)
 	{
 		out << "List is empty";
 		return out;
 	}
+	// Loop through the list
 	do
 	{
+		// Output the data of the current Node followed by a space
 		out << *temp.getCurrent() << " ";
 	}
+	// Keep looping until the end of the loop is reached
 	while (temp.moveNext() == 0);
+	// As current has been manipulated, reset it to head
+	temp.jumpToHead();
 	return out;
 }
 
+// Swap two Nodes in a Linked List
 void LinkedList::swap(Node* a, Node* b)
-{
+{	
+	// Set the next of the first Node to the next of the second Node
 	a->setNext(b->getNext());
+	// Set the next of the second Node to the first Node
 	b->setNext(a);
+	// Set the first Node's next's previous to the first Node
 	if (a->getNext() != NULL)
 	{
 		a->getNext()->setPrev(a);
 	}
 	
+	// Set the previous of the second Node to the previous of the first Node
 	b->setPrev(a->getPrev());
+	// Set the previous of the first Node to the second Node
 	a->setPrev(b);
+	// Set the second Node's previous's Next to the Second Node
 	if (b->getPrev() != NULL)
 	{
 		b->getPrev()->setNext(b);
--- LinkedList.h
+++ LinkedList.h
@@ -1,7 +1,14 @@
+/*************************************
+**** Lachlan Court
+**** c3308061
+**** 13/09/2020
+**** SENG1120 Assignment 1
+**************************************/
+
 #include <iostream>
 using namespace std;
-#ifndef LACHLAN_LINKEDLIST
-#define LACHLAN_LINKEDLIST
+#ifndef LINKEDLIST_CLASS
+#define LINKEDLIST_CLASS
 #include "Node.h"
 
 class LinkedList
@@ -9,33 +16,76 @@ class LinkedList
 	
 	public:
 		typedef Node::value_type value_type;
+		
+		//Constructor
 		LinkedList();
+		//Destructor
 		~LinkedList();
+		
+		//****Queries****//
+		
+		//precondition: None
+		//postcondition: Function returns the size of the Linked List
 		int getSize() const;
-		void add(const value_type data);
-		void addToTail(value_type* data_);
-		void addToHead(value_type* data_);
-		void remove(const value_type identifier);
+		//precondition: None (Empty list possibility is checked during function)
+		//postcondition: Function returns the number of occurrences of the specified identifier
 		int count(const value_type identifier);
+		
+		//****Data manipulation****//
+		
+		//precondition: None
+		//postcondition: The data is stored in the list having been split up into individual words with space delimeter
+		void add(const value_type data);
+		//precondition: None (Empty list possibility is checked during function)
+		//postcondition: Linked List has data added as a new node at the end of the list
+		void addToTail(value_type* data_);
+		//precondition: None (Empty list possibility is checked during function)
+		//postcondition: Linked List has data added as a new node at the start of the list
+		void addToHead(value_type* data_);
+		//precondition: None (Empty list possibility is checked during function)
+		//postcondition: All items that match the specified identifier will be removed from the list
+		void remove(const value_type identifier);
+		//precondition: None (Empty list possibility is checked during function)
+		//postcondition: Linked list will be sorted by alphabetical order
 		void sort();
 		
+		//****List traversing****//
+		
+		//precondition: None
+		//postcondition: Linked List current is set to head, returns -1 if list is empty and 0 if successful
 		int jumpToHead();
+		//precondition: None
+		//postcondition: Linked List current is set to tail, returns -1 if list is empty and 0 if successful
 		int jumpToTail();
+		//precondition: Linked List current cannot be NULL
+		//postcondition: Linked List current is set to the value of it's next, returns -1 if current is now NULL and 0 if successful
 		int moveNext();
+		//precondition: Linked List current cannot be NULL
+		//postcondition: Linked List current is set to the value of it's previous, returns -1 if current is now NULL and 0 if successful
 		int movePrev();
+		//precondition: Linked List current cannot be NULL
+		//postcondition: Returns the pointer to the data of the current Node
 		value_type* getCurrent() const;
 		
-		void operator += (LinkedList& temp);
+		//precondition: temp cannot be NULL but the Linked List itself can be empty
+		//postcondition: Adds the items in the temp Linked List onto the end of the existing Linked List
+		void operator += (LinkedList& otherList);
 	
 	private:
+		//****Instance variables****//
 		Node* head;
 		Node* tail;
 		Node* current;
 		int size;
 		
-		void swap(Node* a, Node* b);
+		//****Private methods****//
 		
+		//precondition: Neither argument can be NULL
+		//postcondition: The Nodes will be swapped in the Linked List by manipulating the next and previous values
+		void swap(Node* a, Node* b);		
 };
 
+//precondition: temp cannot be NULL but the Linked List itself can be empty
+//postcondition: The contents of the Linked List will be printed out, or "List is Empty" if temp is empty
 ostream& operator << (ostream& out, LinkedList& temp);
 #endif
